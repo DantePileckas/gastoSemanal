22362fa566b8388616c84de615219d701ca147dc
Dante Pileckas <pileckas33@gmail.com>
2021-07-28 18:18:38 -0300

    Creation of the List component

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,9 @@
 import React, {Component} from 'react';
 import '../css/App.css';
-import Header from './Header'
-import Formulario from './Formulario'
+import Header from './Header';
+import Formulario from './Formulario';
+import Listado from './Listado';
+
 
 class App extends Component {
   
@@ -13,11 +15,11 @@ state = {
   //Agregar un nuevo gasto al state
   agregarGasto = gasto =>{
     //tomar una copia de state actual
-      const gastos = {...this.state.gastos} //--Con e state.gastos creo una copia del estado actual y lo almaceno en "gastos"
+      const gastos = {...this.state.gastos}; //--Con e state.gastos creo una copia del estado actual y lo almaceno en "gastos"
 
     //agregar al gasto al objeto del state
 
-      gastos[`gastos${Date.now()}`] = gasto;    
+      gastos[`gasto${Date.now()}`] = gasto;    
 
     //ponerlo en state
   
@@ -34,13 +36,15 @@ state = {
   />
   <div className="contenido-principal contenido">
     <div className="row">
-      <div clssName="one-halg column">
+      <div className="one-half column">
         <Formulario 
           agregarGasto = {this.agregarGasto}
         />
       </div>
-      <div clssName="one-halg column">
-
+      <div className="one-half column">
+        <Listado
+          gastos={this.state.gastos}
+        />
       </div>
     </div>
   </div>
